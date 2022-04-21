ab91ff50b9396826cdf9868bc2fd7af97c27981e
LikeAJohny <code@john-at-work.de>
2022-04-21 11:26:18 +0200

    some stylings, init create state & save

--- src/App.vue
+++ src/App.vue
@@ -20,10 +20,10 @@ import { RouterLink, RouterView } from "vue-router";
 @import "@/assets/base.css";
 
 #app {
+  display: grid;
+  place-content: center;
   max-width: 1280px;
   margin: 0 auto;
-  padding: 2rem;
-
   font-weight: normal;
 }
 
@@ -73,43 +73,5 @@ nav a {
 
 nav a:first-of-type {
   border: 0;
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
 }
 </style>
--- src/views/NonoCreate.vue
+++ src/views/NonoCreate.vue
@@ -1,45 +1,49 @@
 <template>
-  <div>
-    <div>
+  <div class="create-form">
+    <div class="form-group">
       <label for="rows">Rows</label>
-      <input type="number" id="rows" v-model="state.rows" />
+      <input type="number" id="rows" v-model="state.rows" tabindex="1" />
     </div>
-    <div>
+    <div class="form-group">
       <label for="cols">Cols</label>
-      <input type="number" id="cols" v-model="state.cols" />
+      <input type="number" id="cols" v-model="state.cols" tabindex="2" />
+    </div>
+    <div class="form-group">
+      <button @click="save" tabindex="3">Save Level</button>
     </div>
   </div>
-  <br />
-  <div>
+  <div class="level">
     <div class="board">
       <div class="row" v-for="(row, ri) in state.grid" :key="ri">
         <div
-          class="cell"
+          :class="{ cell: true, filled: state.grid[ri][ci] === 1 }"
           v-for="(_, ci) in row"
           :key="ci"
           @click="updateCell(ri, ci)"
-        >
-          {{ state.grid[ri][ci] }}
-        </div>
+        ></div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { reactive, watch } from "vue";
+import { onMounted, reactive, watch } from "vue";
 import type { Grid } from "@/types/level";
 
-const state = reactive({
-  rows: 4,
-  cols: 4,
-  grid: new Array(4).fill(0).map(() => new Array(4).fill(0)),
-});
+const state = reactive(initState());
 
 watch(
   () => [state.rows, state.cols],
   () => (state.grid = generateGrid(state.rows, state.cols))
 );
+
+function initState() {
+  return {
+    rows: 4,
+    cols: 4,
+    grid: new Array(4).fill(0).map(() => new Array(4).fill(0)),
+  };
+}
 
 function updateCell(ri: number, ci: number): void {
   state.grid[ri][ci] = state.grid[ri][ci] === 1 ? 0 : 1;
@@ -56,9 +60,31 @@ function generateGrid(rows: number, cols: number): Grid {
         )
     );
 }
+
+function save(): void {
+  console.log(state.grid);
+}
+
+onMounted(() => {
+  document.getElementById("rows")?.focus();
+});
 </script>
 
 <style scoped>
+.create-form {
+  display: flex;
+  column-gap: 1rem;
+  margin-top: 2rem;
+}
+.form-group {
+  display: grid;
+}
+.level {
+  display: grid;
+  place-content: center;
+  width: 100%;
+  margin-top: 2rem;
+}
 .row {
   display: flex;
   place-items: center;
@@ -70,6 +96,9 @@ function generateGrid(rows: number, cols: number): Grid {
   aspect-ratio: 1;
   border: 1px solid;
   cursor: pointer;
-  transition: all 1s ease;
+  transition: all 0.33s ease;
+}
+.cell.filled {
+  background-color: var(--color-text);
 }
 </style>
